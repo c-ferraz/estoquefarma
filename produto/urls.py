79ae5d44fbafc9c0906837654b8070f8f9acfbc9
Johnes Thomas <johnes_brasil10@hotmail.com>
2021-05-06 00:04:57 -0300

    BotaoConsultar

--- produto/urls.py
+++ produto/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('inicio', views.inicio, name='inicio'),
     path('index', views.index, name='index'),
     path('cadastrar_produto', views.cadastrar_produto, name='cadastrar_produto'),
-    path('alocar', views.alocar, name='alocar_produto')
+    path('alocar', views.alocar, name='alocar_produto'),
+    path('consultar', views.consultar_produto, name='consultar_produto'),
+    path('editar/<int:product_id>', views.editar, name='editar'),
+    path('editar_dados_produto', views.editar_dados_produto, name='editar_dados_produto'),
 
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import request
+from django.http import request, HttpResponseRedirect
 from rest_framework import status
 import json
 
@@ -47,3 +47,40 @@ def alocar(request):
 
     return render(request, 'alocar.html')
 
+def consultar_produto(request):
+
+    produtos = Produto.objects.all()
+
+    contexto = {
+        'produtos': produtos
+    }
+
+    return render(request, 'consultar.html', contexto)
+
+def editar(request, product_id):
+
+    if request.method == 'GET':
+        produtos = Produto.objects.filter(codigo_de_barras=product_id)
+        contexto = {
+            'produtos': produtos
+        }
+    return render(request, 'editar.html', contexto)
+
+def editar_dados_produto(request):
+
+    if request.method == 'POST':
+        nome_do_produto = request.POST.get('nomeProduto')
+        codigo_de_barras = request.POST.get('codBarraProduto')
+        lote = request.POST.get('loteProduto')
+        ativo = request.POST.get('ativoProduto')
+        product_id = request.POST.get('product_id')
+
+        if ativo != None:
+            ativo = True
+        else:
+            ativo = False
+
+        produto = Produto.objects.filter(codigo_de_barras=product_id)
+        produto.update(nome_do_produto=nome_do_produto, codigo_de_barras=codigo_de_barras, lote=lote, ativo=ativo)
+
+    return HttpResponseRedirect('/produto/inicio')
